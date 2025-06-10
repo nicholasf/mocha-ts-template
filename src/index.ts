26ac5e8096895f1d69ee7bdcb267d68637fee461
nicholasf <nicholas.faiz@gmail.com>
2025-06-11 09:49:04 +1000

    Import attribute types are terrific

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import { readFileSync } from "fs";
 
-const filePath = "../../test/fixtures/messages.json";
+const filePath = "./test/fixtures/messages.json";
 
 
 export function parseJSON(path: string): any {
-    return {};
+    let data = readFileSync(filePath)
+    try {
+        return JSON.parse(data.toString());
+    } catch (error) {
+        throw new Error(`Failed to parse JSON from ${path}: ${error}`);
+    }
 }
--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,7 @@
 import { expect } from 'chai';
-import { parseJSON } from '../src/index';
+import { parseJSON } from '../src/index.js';
+import kv from './fixtures/key-val.json' with { type: 'json' };
+
 
 describe('hello', () => {
     it("adds things", () => {
@@ -7,23 +9,40 @@ describe('hello', () => {
     });
 })
 
-describe('it parses JSON', async () => {
-    it('should parse a valid JSON file', async () => {
+type messages = {
+    one: boolean;
+    two: {
+        three: boolean;
+        four: number;
+    }
+    three: string
+}
+
+describe('it parses JSON using fs.readFileSync', async () => {
+    it('should parse a valid JSON file using a matching type', async () => {
         const filePath = './test/fixtures/messages.json';
-        const result = await parseJSON(filePath);
+        const result: messages = (parseJSON(filePath) as messages)!;
+        console.log(result)
         expect(result).to.be.an('object');
-        expect(result).to.have.property('messages');
-        expect(result.messages).to.be.an('array');
+        expect(result.one).to.be.true;       
+        expect(result.two.four).to.equal(42);       
     });
 
     it('should throw an error for an invalid JSON file', async () => {
         const filePath = './test/fixtures/invalid.json';
         try {
-            await parseJSON(filePath);
-            throw new Error('Expected an error to be thrown');
+            parseJSON(filePath);
         } catch (err) {
             expect(err).to.be.an('error');
-            expect((err as Error).message).to.include('Unexpected token');
+            expect((err as Error).message).to.include('Failed to parse JSON');
         }
     });
 })
+
+describe('it uses Record<t.v>', () => {})
+
+describe.only('it imports JSON as a module with import attribute json', () => {
+    console.log(kv);
+    expect(kv.one).to.equal(1)
+})
+
